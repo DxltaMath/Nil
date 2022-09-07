59630b891ffe57cf1bd8168bccb3daa750caa0d6
gemsvido <gemsvido@gmail.com>
2022-09-06 17:56:38 -0700

    looking damn good

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,10 @@ import io.javalin.Javalin
 
 fun main (args : Array<String>) {
 
-    val app = Javalin.create().start(x.HTTP_PORT)
-    app.get("/") { ctx -> ctx.result("Hello World") }
+    val app = Javalin.create().start(constants.HTTP_PORT)
+    app.get("/") { ctx ->
+
+        ctx.res.contentType = "text/html"
+        ctx.result(website.INDEX)
+    }
 }
--- src/constants.kt
+++ src/constants.kt
@@ -1,4 +1,7 @@
-object x {
+import java.io.FileReader
+
+/** Constants */
+object constants {
 
      /** Override your own DMIx version here, if not, Nil will use the latest DMIx version. (updated manually by DxltaMath admins)
       * CAUTION: DMIx will prompt to update if the version does not math this one's version. */
@@ -14,11 +17,26 @@ object x {
      * CAUTION: dGUIs have complete access. Only use dGUIs that you trust. */
     const val GUI_LINK : String = "https://raw.githubusercontent.com/DxltaMath/dGUI/master/dist/bundle.js"
 
+
     /** Set this to true if you would like to unminify main.js
      * CAUTION: this will HEAVILY increase the filesize of main.js- use for debugging ONLY. */
     const val UNMINIFY_SOURCE : Boolean = false
 
+
+    /** Override your own License links here, otherwise DxltaMath's license repo will be used
+     * CAUTION: If your license is incompatible with ours, then you are violating our license and may not use DxltaMath software. */
+    const val LICENSE_LINK : String = "https://github.com/DxltaMath/license";
+
+}
+
+/** Website constants */
+object website {
+
     /** Insert your own contents of index.html here, if not Nil's normal index.html will be used.
      * CAUTION: Remember that this is the exported main page of the site. Be wise. */
+    val INDEX : String = FileReader("./html/index.html").readText()
 
+    /** Insert your own contents of style.css here, if not Nil's normal style.css will be used.
+     * CAUTION: This is exported at `<site>/style.css`. Remember to insert a stylesheet link in index.html to use this. */
+    val STYLE : String = FileReader("./html/style.css").readText()
 }
